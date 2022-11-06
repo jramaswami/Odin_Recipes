f8664673fd97c695772890ec80ea8d3a0d35d2cb
Joseph Ramaswami <jramaswami@gmail.com>
2022-11-06 14:25:03 -0500

    Prepare to separate images for each recipe into their own folder
    Revised two_pointers.html and manim/two_pointer/move_image_files.py
    to handle image files in images/two_pointer.

--- manim/two_pointer/move_image_files.py
+++ manim/two_pointer/move_image_files.py
@@ -10,5 +10,5 @@ for i in range(1, 11):
     cmdname = f"manim -s -r 500,281 step{i}.py"
     os.system(cmdname)
     srcname = f"media/images/step{i}/Step{i}_ManimCE_v0.16.0.post0.png"
-    dstname = f"../../images/step{i}.png"
+    dstname = f"../../images/two_pointer/step{i}.png"
     shutil.copyfile(srcname, dstname)
